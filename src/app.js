5710f5acb99063b5bedfe1f9adaad3e981115e53
Anuj <ay517328@gmail.com>
2022-12-25 07:28:06 +0530

    adding env to project

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 require("./db/conn");
 const hbs = require("hbs");
 const app = express();
 const Register = require("./models/registers");
+
+const bcrypt = require("bcryptjs");
+
+const securePassword = async (password) => {
+    const passwordHash = await bcrypt.hash(password, 12);
+    // console.log(passwordHash);
+
+    // const passwordMatch = await bcrypt.compare(password,passwordHash);
+    // console.log(passwordMatch);
+
+}
+securePassword("yadav@12");
+
+
 const port = process.env.PORT || 1200;
 
 // const staticPath = path.join(__dirname,"../public")
@@ -16,6 +31,7 @@ app.set("views", templatePath);
 hbs.registerPartials(partialsPath)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+// console.log(process.env.SECRET_KEY);
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -33,15 +49,18 @@ app.post("/register", async (req, res) => {
         const cpassword = req.body.confirmpassword;
         if (password === cpassword) {
             const registerEmploye = new Register({
-firstname:req.body.firstname,
-lastname:req.body.lastname,
-email:req.body.email,
-password:req.body.password,
-confirmpassword:req.body.confirmpassword
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                password: req.body.password,
+                confirmpassword: req.body.confirmpassword
             })
-         const registered = await registerEmploye.save();
-         res.status(201).render("index");
-        //to view on browser  res.send(registered);
+            console.log("the success part" + registerEmploye);
+            const token = await registerEmploye.generateAuthToken();
+            //   console.log("the token part" + token);
+            const registered = await registerEmploye.save();
+            res.status(201).render("index");
+            //to view on browser  res.send(registered);
         } else {
             res.send('password not same ❌')
         }
@@ -52,30 +71,51 @@ confirmpassword:req.body.confirmpassword
     }
 })
 
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.render("login");
 })
 
+// login check
+app.post("/login", async (req, res) => {
+    try {
 
-app.post("/login",async(req,res)=>{
-try{
+        const email = req.body.email;
+        const password = req.body.password;
+        // console.log(`${email} and password is${password}`);
+        const useremail = await Register.findOne({ email: email })
+        const isMatch = await bcrypt.compare(password, useremail.password);
 
-    const email = req.body.email;
-    const password = req.body.password;
-    // console.log(`${email} and password is${password}`);
-const userEmail = await Register.findOne({email:email})
-// res.send(userEmail);
-// console.log(userEmail);
-if(userEmail.password === password){
-    res.status(201).render("login");
+        const token = await useremail.generateAuthToken();
+        console.log("The token part" + token);
+       
+        if (isMatch) {
+            res.status(201).render("login");
 
-}else{
-    res.send("Invalid Details ❌")
-}
-}catch(error){
-    res.status(400).send("Invalid Email")
-}
+        } else {
+            res.send("Invalid Details ❌")
+        }
+    } catch (error) {
+        res.status(400).send("Invalid Email");
+    }
 })
+
+const jwt = require("jsonwebtoken");
+
+const createToken = async () => {
+    // jwt.sign(payload, secretOrPrivateKey, [options, callback])
+
+    const token = await jwt.sign({ _id: "63a546fb9fdf1cf6d3a71c82" }, "anujyadavishereanujyadadvjourneyhasbegunnownobodycanstopanujyadavexceptanujyadavhimself", {
+        expiresIn: "2 seconds"
+    });
+    console.log(token);
+    //   jwt.verify(token, secretOrPublicKey, [options, callback])
+
+    const userVerify = await jwt.verify(token, "anujyadavishereanujyadadvjourneyhasbegunnownobodycanstopanujyadavexceptanujyadavhimself");
+    console.log(userVerify);
+
+}
+createToken();
+
 app.listen(port, () => {
     console.log(`listening at port ${port}`);
 })
--- src/models/registers.js
+++ src/models/registers.js
@@ -1,31 +1,74 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const employeeSchema = new mongoose.Schema({
-  firstname:{
-    type:String,
-    required:true
+  firstname: {
+    type: String,
+    required: true
   },
-  lastname:{
-    type:String,
-    required:true
+  lastname: {
+    type: String,
+    required: true
+
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+
+  },
+  confirmpassword: {
+    type: String,
+    required: true
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+})
+// generating tokens.
+employeeSchema.methods.generateAuthToken = async function () {
+  try {
     
-  },
-  email:{
-    type:String,
-    required:true,
-    unique:true
-  },
-  password:{
-    type:String,
-    required:true,
-    unique:true
-  },
-  confirmpassword:{
-    type:String,
-    required:true,
-    unique:true
+    const token = jwt.sign({ _id: this._id.toString() }, process.env.SECRET_KEY);
+    // token:token both key & value are some so by object destructuring.
+    this.tokens = this.tokens.concat({ token: token })
+    await this.save()
+    return token;
+  } catch (error) {
+    res.send("The error part" + error);
+    console.log("The error part" + error);
   }
+}
+
+
+// converting password to hash format.
+employeeSchema.pre("save", async function (next) {
+
+  if (this.isModified('password')) {
+    // console.log(`the current password is ${this.password}`);
+
+    this.password = await bcrypt.hash(this.password, 12)
+    this.confirmpassword = await bcrypt.hash(this.password, 12)
+
+    // console.log(`the current password is ${this.confirmpassword}`);
+    // this.confirmpassword = undefined;
+    // console.log(`the current password is ${this.password}`);
+
+  }
+
+
+  next();
 })
 
+
+
 // creating collections:table,documents:rows
-const Register = new mongoose.model("Register",employeeSchema);
+const Register = new mongoose.model("Register", employeeSchema);
 module.exports = Register;
